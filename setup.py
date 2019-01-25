89098e62b5eaddecc81bc08d4c8437f9caa9f0be
Nathan Voss <nathan@finitestate.io>
2019-01-25 13:57:24 -0500

    Added minimal setup.py to enable pip install of this package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+setup(
+    name='fact_core',
+    version=VERSION,
+    packages=find_packages(),
+    description='Finite State fork of minimized FACT_core repo',
+    author='Finite State',
+    author_email='',
+    url='',
+    license='')
